e711b6b6795fe861222ffd278bd99776a555e2a0
Yuichiro Luke Smith <contact@yu-smith.com>
2022-01-29 00:41:03 +0900

    refactor: ⚡️ 03_io add concatenation

--- 03_io/src/main.rs
+++ 03_io/src/main.rs
@@ -1,28 +1,31 @@
-use std::{env, io};
+use std::env;
 use std::fs::File;
-use std::io::BufRead;
+use std::io::{BufRead, BufReader};
 use std::ops::Deref;
 use std::process::exit;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = args
-        .get(1)
-        .map(String::deref)
-        .unwrap_or("/dev/stdin");
+    let args: Vec<String> = env::args().skip(1).collect();
+    let filenames = match args.len() {
+        0 => vec!["/dev/stdin"],
+        _ => args.iter().map(String::deref).collect(),
+    };
 
-    let file = File::open(filename).unwrap_or_else(|_| {
-        println!("Could not open file '{}'", filename);
-        exit(1)
-    });
-    let file = file;
     let mut idx = 0;
 
+    for filename in filenames {
+        let file = File::open(filename).unwrap_or_else(|_| {
+            println!("Could not open file '{}'", filename);
+            exit(1)
+        });
 
-    for line in io::BufReader::new(file).lines() {
-        if let Ok(line) = line {
-            idx = idx + 1;
-            println!("{}| {}", idx, line);
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                idx = idx + 1;
+                println!("{}| {}", idx, line);
+            }
         }
     }
 }
